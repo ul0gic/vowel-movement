fa5968a945040d3d83010572e7b70fae1b6bd32b
dev-corelift <devteam@corelift.io>
2026-01-28 09:12:25 -0600

    Fix worker.ts TypeScript types

--- src/worker.ts
+++ src/worker.ts
@@ -1,5 +1,5 @@
 export default {
-  async fetch(request: Request, env: { ASSETS: Fetcher }): Promise<Response> {
+  async fetch(request: Request, env: { ASSETS: { fetch: typeof fetch } }): Promise<Response> {
     return env.ASSETS.fetch(request);
   },
 };
